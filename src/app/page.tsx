cafa354a6065578bab06d4cab77b961fdf780383
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-02-16 02:42:53 -0500

    188. NextJS Fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image';
+import { titleFont } from '@/config/fonts';
 
 export default function Home() {
 	return (
 		<main className="">
 			<h1>Hola Mundo</h1>
+			<h1 className={`${titleFont.className} font-bold`}>Hola Mundo</h1>
+			<h1 className={`${titleFont.className}`}>Hola Mundo</h1>
 		</main>
 	);
 }
