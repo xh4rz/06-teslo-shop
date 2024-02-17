b4b64728d003f04c2d809e62fa8c0c606692589d
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-02-17 02:43:52 -0500

    191. TopMenu

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -1,7 +1,15 @@
+import { TopMenu } from '@/components';
+
 export default function ShopLayout({
 	children
 }: {
 	children: React.ReactNode;
 }) {
-	return <main className="min-h-screen bg-red-500">{children}</main>;
+	return (
+		<main className="min-h-screen">
+			<TopMenu />
+
+			{children}
+		</main>
+	);
 }
