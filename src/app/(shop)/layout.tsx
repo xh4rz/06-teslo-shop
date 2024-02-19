5c6a7a1e3ab96ae2faee8407ff411a517309a134
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-02-19 12:16:25 -0500

    194. Componente - Title

--- src/app/(shop)/layout.tsx
+++ src/app/(shop)/layout.tsx
@@ -6,10 +6,10 @@ export default function ShopLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<main className="min-h-screen">
+		<main className="min-h-screen ">
 			<TopMenu />
 
-			{children}
+			<div className="px-0 sm:px-10">{children}</div>
 		</main>
 	);
 }
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,9 @@
-import { titleFont } from '@/config/fonts';
+import { Title } from '@/components';
 
 export default function Home() {
 	return (
-		<div>
-			<h1>Hola Mundo</h1>
-			<h1 className={`${titleFont.className} font-bold`}>Hola Mundo</h1>
-			<h1 className={`${titleFont.className}`}>Hola Mundo</h1>
-		</div>
+		<>
+			<Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
+		</>
 	);
 }
