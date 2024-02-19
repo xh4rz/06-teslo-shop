2183bf5d38086071692933700ca4306297ad1f04
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-02-19 15:34:19 -0500

    198. Menú lateral - Primera parte

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export * from './ui/not-found/PageNotFound';
 export * from './ui/title/Title';
 export * from './products/product-grid/ProductGrid';
 export * from './products/product-grid/ProductGridItem';
+export * from './ui/sidebar/Sidebar';
