e5bd99a1913d8d1171f9f3ba96a621d94ac21ce8
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-02-19 21:08:17 -0500

    206. Tarea - Pantalla de categorias

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,20 +1,45 @@
-import { notFound } from 'next/navigation';
+// import { notFound } from 'next/navigation';
+import { initialData } from '@/seed/seed';
+import { ProductGridItem, Title } from '@/components';
+import { Category } from '@/interfaces';
+
+const seedProducts = initialData.products;
 
 interface Props {
 	params: {
-		id: string;
+		id: Category;
 	};
 }
 
 export default function CategoryPage({ params }: Props) {
 	const { id } = params;
 
-	if (id === 'kids') {
-		notFound();
-	}
+	const products = seedProducts.filter((i) => i.gender === id);
+
+	const labels: Record<Category, string> = {
+		men: 'para hombres',
+		women: 'para mujeres',
+		kid: 'para niños',
+		unisex: 'para todos'
+	};
+
+	// if (id === 'kids') {
+	// 	notFound();
+	// }
+
 	return (
-		<div>
-			<h1>Category Page {id}</h1>
-		</div>
+		<>
+			<Title
+				title={`Artículos ${labels[id]}`}
+				subtitle="Todos los productos"
+				className="mb-2"
+			/>
+
+			<div className="grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10">
+				{products.map((product) => (
+					<ProductGridItem key={product.slug} product={product} />
+				))}
+			</div>
+		</>
 	);
 }
