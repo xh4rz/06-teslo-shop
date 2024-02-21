95d5a3d84109984a3da63f69359a847eb5720828
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-02-21 00:03:27 -0500

    208. Selector de tallas

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { SizeSelector } from '@/components';
 import { titleFont } from '@/config/fonts';
 import { initialData } from '@/seed/seed';
 import { notFound } from 'next/navigation';
@@ -29,6 +30,10 @@ export default function ProductPage({ params }: Props) {
 				<p className="text-lg mb-5">{product.price}</p>
 
 				{/* Selector de Tallas */}
+				<SizeSelector
+					selectedSize={product.sizes[0]}
+					availableSizes={product.sizes}
+				/>
 
 				{/* Selector de Cantidad */}
 
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export * from './ui/title/Title';
 export * from './products/product-grid/ProductGrid';
 export * from './products/product-grid/ProductGridItem';
 export * from './ui/sidebar/Sidebar';
+export * from './product/size-selector/SizeSelector';
