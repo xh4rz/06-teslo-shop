4764e06545f294857d988a4c99dc6af62bb2ee9b
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-02-23 02:57:19 -0500

    213. Slideshow mobile

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,4 +1,9 @@
-import { ProductSlideshow, QuantitySelector, SizeSelector } from '@/components';
+import {
+	ProductMobileSlideshow,
+	ProductSlideshow,
+	QuantitySelector,
+	SizeSelector
+} from '@/components';
 import { titleFont } from '@/config/fonts';
 import { initialData } from '@/seed/seed';
 import { notFound } from 'next/navigation';
@@ -21,7 +26,19 @@ export default function ProductPage({ params }: Props) {
 		<div className="mt-5 mb-20 grid grid-cols-1 md:grid-cols-3 gap-3">
 			{/* Slideshow */}
 			<div className="col-span-1 md:col-span-2">
-				<ProductSlideshow title={product.title} images={product.images} />
+				{/* Mobile Slideshow */}
+				<ProductMobileSlideshow
+					title={product.title}
+					images={product.images}
+					className="block md:hidden"
+				/>
+
+				{/* Desktop Slideshow */}
+				<ProductSlideshow
+					title={product.title}
+					images={product.images}
+					className="hidden md:block"
+				/>
 			</div>
 
 			{/* Detales */}
--- src/components/index.ts
+++ src/components/index.ts
@@ -7,3 +7,4 @@ export * from './ui/sidebar/Sidebar';
 export * from './product/size-selector/SizeSelector';
 export * from './product/quantity-selector/QuantitySelector';
 export * from './product/slideshow/ProductSlideshow';
+export * from './product/slideshow/ProductMobileSlideshow';
