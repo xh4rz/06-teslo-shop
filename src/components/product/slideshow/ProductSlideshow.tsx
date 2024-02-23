6fae3d33e706756d5db0a7de6c574f808603c54f
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-02-23 02:33:52 -0500

    212. Slideshow con Thumbs - Parte 2

--- src/components/product/slideshow/ProductSlideshow.tsx
+++ src/components/product/slideshow/ProductSlideshow.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { Swiper as SwiperObject } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+import { Autoplay, FreeMode, Navigation, Thumbs } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/free-mode';
@@ -24,16 +24,17 @@ export const ProductSlideshow = ({ images, title, className }: Props) => {
 	return (
 		<div className={className}>
 			<Swiper
-				style={
-					{
-						'--swiper-navigation-color': '#fff',
-						'--swiper-pagination-color': '#fff'
-					} as React.CSSProperties
-				}
+				// style={
+				// 	{
+				// 		'--swiper-navigation-color': '#fff',
+				// 		'--swiper-pagination-color': '#fff'
+				// 	} as React.CSSProperties
+				// }
 				spaceBetween={10}
 				navigation={true}
+				autoplay={{ delay: 2500 }}
 				thumbs={{ swiper: thumbsSwiper }}
-				modules={[FreeMode, Navigation, Thumbs]}
+				modules={[FreeMode, Navigation, Thumbs, Autoplay]}
 				className="mySwiper2"
 			>
 				{images.map((image) => (
@@ -41,6 +42,28 @@ export const ProductSlideshow = ({ images, title, className }: Props) => {
 						<Image
 							width={1024}
 							height={800}
+							src={`/products/${image}`}
+							alt={title}
+							className="rounded-lg object-fill"
+						/>
+					</SwiperSlide>
+				))}
+			</Swiper>
+
+			<Swiper
+				onSwiper={setThumbsSwiper}
+				spaceBetween={10}
+				slidesPerView={4}
+				freeMode={true}
+				watchSlidesProgress={true}
+				modules={[FreeMode, Navigation, Thumbs]}
+				className="mySwiper"
+			>
+				{images.map((image) => (
+					<SwiperSlide key={image}>
+						<Image
+							width={300}
+							height={300}
 							src={`/products/${image}`}
 							alt={title}
 							className="rounded-lg object-fill"
