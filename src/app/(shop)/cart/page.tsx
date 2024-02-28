17e55b92281e7785d65a984fb01e8218470d37c5
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-02-28 00:17:17 -0500

    223. Detalle estético del carrito de compras

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -52,7 +52,7 @@ export default function CartPage() {
 					</div>
 
 					{/* Checkout - Resumen de orden */}
-					<div className="bg-white rounded-xl shadow-xl p-7">
+					<div className="bg-white rounded-xl shadow-xl p-7 h-fit">
 						<h2 className="text-2xl mb-2">Resumen de orden</h2>
 
 						<div className="grid grid-cols-2">
