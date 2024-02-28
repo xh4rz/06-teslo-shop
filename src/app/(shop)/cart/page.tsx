2c43a8b1ec6daa03f10991de079ef5af9969c978
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-02-27 22:22:13 -0500

    220. Pantalla de carrito de compras vacío

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { initialData } from '@/seed/seed';
 import { QuantitySelector, Title } from '@/components';
+// import { redirect } from 'next/navigation';
 
 const productsInCart = [
 	initialData.products[0],
@@ -10,6 +11,8 @@ const productsInCart = [
 ];
 
 export default function CartPage() {
+	// redirect('/empty');
+
 	return (
 		<div className="flex justify-center items-center mb-72 px-10 sm:px-0">
 			<div className="flex flex-col w-[1000px]">
