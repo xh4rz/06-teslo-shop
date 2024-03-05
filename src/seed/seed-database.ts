fd41c42c21a2c85568973c2457eaa633044fa7f5
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-03-04 22:07:00 -0500

    237. Semilla de base de datos - Relación Productos - Categorías

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -19,6 +19,18 @@ async function main() {
 	await prisma.category.createMany({
 		data: categoriesData
 	});
+
+	const categoriesDB = await prisma.category.findMany();
+
+	const categoriesMap = categoriesDB.reduce((map, category) => {
+		map[category.name.toLowerCase()] = category.id;
+
+		return map;
+	}, {} as Record<string, string>); //<string=shirt, string=categoryID>
+
+	console.log(categoriesMap);
+
+	// Productos
 }
 
 (() => {
