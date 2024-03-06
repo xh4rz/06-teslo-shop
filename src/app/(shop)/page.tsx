ee8d3e1b7031a284edcda68007ba9a10afc8a409
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-03-06 00:35:15 -0500

    245. Cargar productos de base de datos

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,11 @@
+import { getPaginatedProductsWithImages } from '@/actions';
 import { ProductGrid, Title } from '@/components';
 import { initialData } from '@/seed/seed';
 
 const products = initialData.products;
 
-export default function Home() {
+export default async function Home() {
+	const productsTemp = await getPaginatedProductsWithImages();
 	return (
 		<>
 			<Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
