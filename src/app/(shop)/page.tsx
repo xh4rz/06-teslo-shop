0c546db2bf5cd53a552c3d5f2bc79b193f15b370
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-03-06 01:24:47 -0500

    250. Paginación - Componente HTML

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,5 +1,5 @@
 import { getPaginatedProductsWithImages } from '@/actions';
-import { ProductGrid, Title } from '@/components';
+import { Pagination, ProductGrid, Title } from '@/components';
 import { redirect } from 'next/navigation';
 
 interface Props {
@@ -25,6 +25,8 @@ export default async function Home({ searchParams }: Props) {
 			<Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
 
 			<ProductGrid products={products} />
+
+			<Pagination totalPages={totalPages} />
 		</>
 	);
 }
