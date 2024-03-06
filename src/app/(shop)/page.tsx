f40f614eb37ea0f332b0568900671131225fbaf0
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-03-06 00:58:28 -0500

    248. Controlar cuando no hay productos en la página

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,5 +1,6 @@
 import { getPaginatedProductsWithImages } from '@/actions';
 import { ProductGrid, Title } from '@/components';
+import { redirect } from 'next/navigation';
 
 interface Props {
 	searchParams: {
@@ -11,6 +12,10 @@ export default async function Home({ searchParams }: Props) {
 	const page = searchParams.page ? parseInt(searchParams.page) : 1;
 
 	const { products } = await getPaginatedProductsWithImages({ page });
+
+	if (products.length === 0) {
+		redirect('/');
+	}
 
 	return (
 		<>
