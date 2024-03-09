8589681b4f62581b672d18e6cd07cb1486dfbdef
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-03-09 14:46:57 -0500

    280. Hacer persistente el carrito de compras

--- src/store/cart/cart-store.ts
+++ src/store/cart/cart-store.ts
@@ -1,5 +1,6 @@
 import { CartProduct } from '@/interfaces';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface State {
 	cart: CartProduct[];
@@ -9,32 +10,37 @@ interface State {
 	// removeProduct
 }
 
-export const useCartStore = create<State>()((set, get) => ({
-	cart: [],
+export const useCartStore = create<State>()(
+	persist(
+		(set, get) => ({
+			cart: [],
 
-	// Methods
-	addProductToCart: (product: CartProduct) => {
-		const { cart } = get();
+			// Methods
+			addProductToCart: (product: CartProduct) => {
+				const { cart } = get();
 
-		// 1. Revisar si el producto existe en el carrito con la talla seleccionada
-		const productInCart = cart.some(
-			(item) => item.id === product.id && item.size === product.size
-		);
+				// 1. Revisar si el producto existe en el carrito con la talla seleccionada
+				const productInCart = cart.some(
+					(item) => item.id === product.id && item.size === product.size
+				);
 
-		if (!productInCart) {
-			set({ cart: [...cart, product] });
-			return;
-		}
+				if (!productInCart) {
+					set({ cart: [...cart, product] });
+					return;
+				}
 
-		// 2. Se que el producto existe por talla... tengo que incrementar
-		const updatedCartProducts = cart.map((item) => {
-			if (item.id === product.id && item.size === product.size) {
-				return { ...item, quantity: item.quantity + product.quantity };
+				// 2. Se que el producto existe por talla... tengo que incrementar
+				const updatedCartProducts = cart.map((item) => {
+					if (item.id === product.id && item.size === product.size) {
+						return { ...item, quantity: item.quantity + product.quantity };
+					}
+
+					return item;
+				});
+
+				set({ cart: updatedCartProducts });
 			}
-
-			return item;
-		});
-
-		set({ cart: updatedCartProducts });
-	}
-}));
+		}),
+		{ name: 'shopping-cart' }
+	)
+);
