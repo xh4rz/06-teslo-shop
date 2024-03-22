e8b7a92c5df4d1e6c8e76ad3f1e6ab52043fba8c
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-03-22 00:41:20 -0500

    301. Cerrar sesión

--- src/components/ui/sidebar/Sidebar.tsx
+++ src/components/ui/sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { logout } from '@/actions';
 import { useUIStore } from '@/store';
 import clsx from 'clsx';
 import Link from 'next/link';
@@ -81,13 +82,14 @@ export const Sidebar = () => {
 					<IoLogInOutline size={30} />
 					<span className="ml-3 text-xl">Ingresar</span>
 				</Link>
-				<Link
-					href="/"
-					className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+
+				<button
+					className="flex w-full items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all"
+					onClick={() => logout()}
 				>
 					<IoLogOutOutline size={30} />
 					<span className="ml-3 text-xl">Salir</span>
-				</Link>
+				</button>
 
 				{/* Line Separator */}
 				<div className="w-full h-px bg-gray-200 my-10" />
