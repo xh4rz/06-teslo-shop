2cd366c13ac69ffc33a80bd69b237dc9a1f42c20
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-21 19:45:05 -0500

    330. Nota de actualización

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -5,6 +5,7 @@ import { countries } from './seed-countries';
 async function main() {
 	// 1. Borrar registros previos
 	// await Promise.all([
+	await prisma.userAddress.deleteMany();
 	await prisma.user.deleteMany();
 	await prisma.country.deleteMany();
 
