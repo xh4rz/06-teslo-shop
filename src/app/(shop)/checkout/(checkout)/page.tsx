4945e07962768517862109932694492f0bd49168
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-04-23 11:03:03 -0500

    336. Pantalla de confirmación - Parte 1

--- src/app/(shop)/checkout/(checkout)/page.tsx
+++ src/app/(shop)/checkout/(checkout)/page.tsx
@@ -1,14 +1,7 @@
 /* eslint-disable react/no-unescaped-entities */
 import Link from 'next/link';
-import Image from 'next/image';
-import { initialData } from '@/seed/seed';
 import { Title } from '@/components';
-
-const productsInCart = [
-	initialData.products[0],
-	initialData.products[1],
-	initialData.products[2]
-];
+import { ProductsInCart } from './ui/ProductsInCart';
 
 export default function CheckoutPage() {
 	return (
@@ -25,27 +18,7 @@ export default function CheckoutPage() {
 						</Link>
 
 						{/* Items */}
-						{productsInCart.map((product) => (
-							<div key={product.slug} className="flex mb-5">
-								<Image
-									src={`/products/${product.images[0]}`}
-									width={100}
-									height={100}
-									style={{
-										width: '100px',
-										height: '100px'
-									}}
-									alt={product.title}
-									className="mr-5 rounded"
-								/>
-
-								<div>
-									<p>{product.title}</p>
-									<p>${product.price} x 3</p>
-									<p className="font-bold">Subtotal: ${product.price * 3}</p>
-								</div>
-							</div>
-						))}
+						<ProductsInCart />
 					</div>
 
 					{/* Checkout - Resumen de orden */}
