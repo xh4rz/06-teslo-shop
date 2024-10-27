785a77e03f0d946a4e8ab549263e0d48d36bf693
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-10-26 22:58:22 -0500

    361. Mostrar Skeleton mientras se carga

--- src/components/paypal/PayPalButton.tsx
+++ src/components/paypal/PayPalButton.tsx
@@ -1,7 +1,18 @@
 'use client';
 
-import { PayPalButtons } from '@paypal/react-paypal-js';
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
 
 export const PayPalButton = () => {
+	const [{ isPending }] = usePayPalScriptReducer();
+
+	if (isPending) {
+		return (
+			<div className="animate-pulse mb-16">
+				<div className="h-11 bg-gray-300 rounded" />
+				<div className="h-11 bg-gray-300 rounded mt-2" />
+			</div>
+		);
+	}
+
 	return <PayPalButtons />;
 };
