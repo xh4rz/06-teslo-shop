261a6607f46566baba7c2d85700289736eea5ac7
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-11-03 02:43:52 -0500

    387. Actualizar producto

--- src/actions/product/create-update-product.ts
+++ src/actions/product/create-update-product.ts
@@ -1,7 +1,8 @@
 'use server';
 
-import { Gender } from '@prisma/client';
+import { Gender, Product, Size } from '@prisma/client';
 import { z } from 'zod';
+import prisma from '@/lib/prisma';
 
 const productSchema = z.object({
 	id: z.string().uuid().optional().nullable(),
@@ -32,7 +33,39 @@ export const createUpdateProduct = async (formData: FormData) => {
 		return { ok: false };
 	}
 
-	console.log(productParsed.data);
+	const product = productParsed.data;
+
+	product.slug = product.slug.toLowerCase().replace(/ /g, '-').trim();
+
+	const { id, ...rest } = product;
+
+	const prismaTx = await prisma.$transaction(async (tx) => {
+		let product: Product;
+
+		const tagsArray = rest.tags
+			.split(',')
+			.map((tag) => tag.trim().toLowerCase());
+
+		if (id) {
+			// actualizar
+			product = await prisma.product.update({
+				where: { id },
+				data: {
+					...rest,
+					sizes: {
+						set: rest.sizes as Size[]
+					},
+					tags: {
+						set: tagsArray
+					}
+				}
+			});
+
+			console.log({ product });
+		} else {
+			// crear
+		}
+	});
 
 	return {
 		ok: true
